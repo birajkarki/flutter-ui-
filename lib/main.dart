fe0344da987c1e5dcc63bab1dc17d7f2197b400e
Biraj Karki <birajkarki9849@gmail.com>
2022-02-05 10:01:46 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
